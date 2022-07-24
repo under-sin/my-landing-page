d0e06d7d6d947bc898eedcbeda682e671e349908
Anderson Vieira do Nascimento <andersonvieira818@yahoo.com.br>
2022-07-24 14:23:14 +0000

    Update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Header />
       <h1>Hello Vite React Starter</h1>
       <GlobalStyle />
     </ThemeProvider>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import {HeaderContainer} from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>UNDER.</span>
+      <nav>
+          <p>Home</p>
+          <p>Projetos</p>
+          <p>Blog</p>
+      </nav>
+    </HeaderContainer>
+  );
+}
