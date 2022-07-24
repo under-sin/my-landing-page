3cf8ff018b408e5a303759349d41f99ea682b197
Anderson Vieira do Nascimento <andersonvieira818@yahoo.com.br>
2022-07-24 18:59:52 +0000

    feat: animação do background adicionada

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,9 +3,40 @@ import styled from "styled-components";
 export const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
+  padding: 1.5rem 0;
 
   nav {
     display: flex;
-    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
+
+    a {
+      padding: 2px;
+
+      text-decoration: none;
+      font-size: 1rem;
+      font-weight: 500;
+      color: ${(props) => props.theme["main-text-color"]};
+      cursor: pointer;
+
+      border-top: 2px solid transparent;
+      border-bottom: 2px solid transparent;
+    }
+
+    a:hover {
+      border-bottom-color: ${(props) => props.theme.pupler};
+    }
+  }
+`;
+
+export const Logo = styled.span`
+  font-size: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  a {
+    text-decoration: none;
+    color: ${(props) => props.theme["main-text-color"]};
   }
 `;
