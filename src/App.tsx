43889fa4c3f77c0f795dba6646b4c80047f12421
Anderson Vieira do Nascimento <andersonvieira818@yahoo.com.br>
2022-07-24 14:22:53 +0000

    feat: montando a estrutura da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-function App() {
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello Vite React Starter</h1>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    boz-sizing: border-box;
+  }
+  
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: ${(props) => props.theme["background-default"]};
+    color: ${(props) => props.theme["main-text-color"]}
+  }
+  
+  body, button, input, textarea {
+    font-family: 'Manrope', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
